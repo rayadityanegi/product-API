53a054b0b1d75200888fefcce87df5cd84dcfd33
rayadityanegi <85740399+rayadityanegi@users.noreply.github.com>
2025-11-10 09:03:21 +0530

    Refactor product API with new endpoints and structure

--- product_api.js
+++ product_api.js
@@ -1,23 +1,49 @@
 import express from "express";
 import fs from "fs";
 
-const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Welcome Home Page");
+const PORT = 3000;
+
+const server = express();
+server.use(express.json());
+
+server.get("/home", (req, res) => {
+  res.end("hi from home API");
 });
-app.get("/product", (req, res) => {
-  fs.readFile("product.json", "utf-8", (err, data) => {
-    if (err) {  
-      res.status(500).send("Error reading products data");
-    } else {
-      const products = JSON.parse(data);
-      res.json(products);
+
+server.get("/product", (req, res) => {
+  fs.readFile("./product.json", "utf-8", (err, data) => {
+    if (err) {
+      res.status(500).send("error in reading file");
+      return;
     }
-  });
+    const products = JSON.parse(data);
+    res.send(products);
+});
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+server.get("/product/:id", (req, res) => {
+  const productId = req.params.id;
+  fs.readFile("./product.json", "utf-8", (err, data) => {
+    if (err) {
+      res.status(500).send("error in reading file");
+      return;
+    }
+    const products = JSON.parse(data);
+    const product = products.find(p => p.id == productId);
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).send("Product not found");
+    }
+});
+});
+
+server.post("/login", (req, res) => {
+  res.end("user created");
+});
+
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
